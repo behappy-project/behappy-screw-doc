db6b123cc0547bf2c9d97ced52473cf7ba2a6775
root <943915349@qq.com>
2022-08-10 17:25:49 +0800

    更新maven版本

--- buildProject.sh
+++ buildProject.sh
@@ -14,11 +14,11 @@ wget https://npm.taobao.org/mirrors/node/v12.22.0/node-v12.22.0-linux-x64.tar.xz
 tar -xvf node-v12.22.0-linux-x64.tar.xz
 ln -sf ${workdir}/node-v12.22.0-linux-x64/bin/node /usr/local/bin/node
 ln -sf ${workdir}/node-v12.22.0-linux-x64/bin/npm /usr/local/bin/npm
-# 下载maven3.8.5
-wget --no-check-certificate https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz
-tar -xzvf apache-maven-3.8.5-bin.tar.gz
+# 下载maven3.8.6
+wget --no-check-certificate https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
+tar -xzvf apache-maven-3.8.6-bin.tar.gz
 cat >> /etc/profile << EOF
-MAVEN_HOME=${workdir}/apache-maven-3.8.5
+MAVEN_HOME=${workdir}/apache-maven-3.8.6
 PATH=$PATH:\$MAVEN_HOME/bin
 export PATH
 EOF
